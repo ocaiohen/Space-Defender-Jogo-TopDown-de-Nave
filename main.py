1e78b0b5f28c344b9652bd80992dafd30c44ba7b
ocaiohen <caiohencontato@gmail.com>
2023-12-23 15:31:40 -0300

    Agora a bala está saindo da ponta da nave. Som de tiro adicionado.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame import mixer
 import math
 
 pygame.init()
@@ -7,16 +8,21 @@ screenWidth = 1280
 screenHeight = 720
 screen = pygame.display.set_mode((screenWidth, screenHeight))
 clock = pygame.time.Clock()
+fps = 60
+
+mixer.init()
+playerShotSound = mixer.Sound("./Sounds/170161__timgormly__8-bit-laser.mp3")
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.playerSize = 0.1
-        self.playerPosition = pygame.math.Vector2(0, 200)
-        self.image = pygame.transform.rotozoom(pygame.image.load("./Sprites/Esboço nave girada pro lado.png").convert_alpha(), 0, self.playerSize)
-        self.image.set_colorkey((255, 255, 255))
+        self.playerPosition = pygame.math.Vector2(screenWidth / 2, screenHeight / 2)
+        self.image = pygame.transform.rotozoom(pygame.image.load("./Sprites/Nave Nova Girada Para Direita PNG.png").convert_alpha(), 0, self.playerSize)
         self.baseImage = self.image
         self.rect = self.image.get_rect(center=self.playerPosition)
+        self.timeOfLastShot = 0
+        self.shotCooldown = fps / 3
         self.speed = 5
 
     def userInput(self):
@@ -38,9 +44,13 @@ class Player(pygame.sprite.Sprite):
             self.velocityX /= (2) ** (1/2)
             self.velocityY /= (2) ** (1/2)
 
+        if pygame.mouse.get_pressed() == (1, 0, 0) and self.timeOfLastShot >= self.shotCooldown:
+            self.shoot()
+            self.timeOfLastShot = 0
+
     def move(self):
         self.playerPosition += pygame.math.Vector2(self.velocityX, self.velocityY)
-        self.rect.center = self.playerPosition  # Atualizar o centro do retângulo com a nova posição
+        self.rect.center = self.playerPosition
 
     def playerRotation(self):
         self.mouseCoordinates = pygame.mouse.get_pos()
@@ -49,14 +59,59 @@ class Player(pygame.sprite.Sprite):
         self.angle = math.degrees(math.atan2(-self.deltaY, self.deltaX))
         self.image = pygame.transform.rotate(self.baseImage, self.angle)
         self.image.set_colorkey((255, 255, 255))
-        self.rect = self.image.get_rect(center=self.rect.center)  # Manter o centro do retângulo após a rotação
+        self.rect = self.image.get_rect(center=self.rect.center)
+
+    def getRotatedPoint(self, relativePoint):
+        rotatedVector = pygame.math.Vector2(relativePoint).rotate(-self.angle)
+        return self.rect.center + rotatedVector
+
+    def shoot(self):
+        bulletStartPoint = self.getRotatedPoint((self.image.get_width() / 2, 0))  # Extremidade no meio do lado direito
+        bullet = Bullet(bulletStartPoint.x, bulletStartPoint.y, -self.angle)
+        bulletsGroup.add(bullet)
+        allSpritesGroup.add(bullet)
+        playerShotSound.play()
 
     def update(self):
         self.userInput()
         self.move()
         self.playerRotation()
+        self.timeOfLastShot += 1
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, startPositionX, startPositionY, angle):
+        super().__init__()
+        self.startPositionX, self.startPositionY, self.angle = startPositionX, startPositionY, angle
+        self.x, self.y = self.startPositionX, self.startPositionY
+        self.image = pygame.image.load("./Sprites/Bullet.png").convert_alpha()
+        self.image = pygame.transform.rotozoom(self.image, 0, 0.03)
+        self.image.set_colorkey((255, 255, 255))
+        self.rect = self.image.get_rect()
+        self.rect.center = (self.x, self.y)
+        self.speed = 9
+        self.velocityY = math.sin(self.angle * (2 * math.pi / 360)) * self.speed
+        self.velocityX = math.cos(self.angle * (2 * math.pi / 360)) * self.speed
+        self.lifetime = fps * 3.5 
+        self.age = 0
+
+    def move(self):
+        self.x += self.velocityX
+        self.y += self.velocityY
+
+        self.rect.x, self.rect.y = self.x, self.y
+    def getOld(self):
+        self.age += 1
+        if self.age >= self.lifetime: self.kill()
+    def update(self):
+        self.move()
+        self.getOld()
+        
 
 player = Player()
+
+allSpritesGroup = pygame.sprite.Group()
+bulletsGroup = pygame.sprite.Group()
+allSpritesGroup.add(player)
 
 endTheGame = False
 while not endTheGame:
@@ -65,10 +120,10 @@ while not endTheGame:
             endTheGame = True
 
     screen.fill((0, 0, 0))
-    screen.blit(player.image, player.rect)
-    player.update()
+    allSpritesGroup.draw(screen)
+    allSpritesGroup.update()
 
     pygame.display.update()
-    clock.tick(60)
+    clock.tick(fps)
 
 pygame.quit()
